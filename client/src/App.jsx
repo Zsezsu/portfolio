da2109f063ec9940ef233b735c0ede8edabfd05e
Zsezsu <88768540+Zsezsu@users.noreply.github.com>
2025-01-08 09:30:08 +0100

    Add basic client folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AboutMe from './components/AboutMe'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome on my Portfolio Site</h1>
+      <AboutMe />
+    </>
+  )
+}
+
+export default App

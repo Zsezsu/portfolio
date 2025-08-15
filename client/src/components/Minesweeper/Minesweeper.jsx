427396a9085b2a21f8c2dd6a3e65ce330a49560d
Zsezsu <88768540+Zsezsu@users.noreply.github.com>
2025-08-15 10:09:04 +0200

    Implement win condition and refactor handleClick logic

--- client/src/components/Minesweeper/Minesweeper.jsx
+++ client/src/components/Minesweeper/Minesweeper.jsx
@@ -8,18 +8,26 @@ import "./minesweeper.css";
 import { useStopwatch } from "./hooks/useStopwatch";
 
 const SIZE = 10;
+const TOTAL_MINES = SIZE;
+const MAX_FIELD_NUMBER = SIZE * SIZE;
+const MAX_HIDDEN_FIELDS = MAX_FIELD_NUMBER - TOTAL_MINES;
+const gameStates = {
+  ready: "ready",
+  running: "running",
+  won: "won",
+  lost: "lost",
+}
 
 function Minesweeper() {
   const [gameBoard, setGameBoard] = useState(null);
   const [gameOver, setGameOver] = useState(false);
   const [gameStarted, setGameStarted] = useState(false);
-  // TODO: need a counter state, which should start from zero, and end size*size
-  // at every click we count +1 if we reach the maximum, and the game is not over, we won
-  const [gameState, setGameState] = useState("ready"); // ready|running|won|lost
+  const [gameState, setGameState] = useState(gameStates.ready);
   const [flagsPlaced, setFlagsPlaced] = useState(0);
-  const totalMines = SIZE;
-  const minesLeft = Math.max(totalMines - flagsPlaced, 0);
+  const minesLeft = Math.max(TOTAL_MINES - flagsPlaced, 0);
   const { elapsed, reset: resetTimer } = useStopwatch(gameState === "running");
+
+  console.log(elapsed);
 
   useEffect(() => {
     const board = generateBoard(SIZE);
@@ -29,24 +37,32 @@ function Minesweeper() {
 
   const handleFirstReveal = () => {
     if (gameState === "ready") {
-      setGameState("running");
+      setGameState(gameStates.running);
       setGameStarted(true);
     }
   };
 
-  const handleWin = () => setGameState("won");
-  const handleLose = () => setGameState("lost");
+  const handleWin = () => {
+    setGameState(gameStates.won);
+    setGameOver(true);
+  }
+
+  const handleLose = () => {
+    setGameState(gameStates.lost);
+    setGameOver(true);
+  }
 
   const restart = () => {
     setGameBoard(generateBoard(SIZE));
     resetTimer();
     setFlagsPlaced(0);
-    setGameState("ready");
+    setGameState(gameStates.ready);
     setGameStarted(false);
     setGameOver(false);
   };
 
-  function displayMines(row, col) {
+  function displayMines(id) {
+    //TODO: refactor the row-col structure and conversion
     const displayBoard = gameBoard.map((row) =>
       row.map((field) =>
         field.isMine
@@ -59,27 +75,44 @@ function Minesweeper() {
             }
       )
     );
-    displayBoard[row][col].exploded = true;
+    displayBoard[id[0]][id[1]].exploded = true;
     setGameBoard(displayBoard);
+  }
+
+  function checkWin(actualBoard) {
+    const count = actualBoard.reduce((hiddenFieldsCount, row) => {
+      return (hiddenFieldsCount += row.reduce(
+        (countOnRow, field) => (!field.hidden ? (countOnRow += 1) : countOnRow),
+        0
+      ));
+    }, 0);
+    return count === MAX_HIDDEN_FIELDS;
   }
 
   function handleClick(id, isGameOver) {
     if (!gameStarted) handleFirstReveal();
-    const row = parseInt(id[0]);
-    const col = parseInt(id[1]);
-    const clickedField = gameBoard[row][col];
 
     if (isGameOver) {
-      setGameOver(true);
-      displayMines(row, col);
       handleLose();
-    } else if (clickedField.isEmpty) {
-      const board = openEmptyFields(row, col);
-      setGameBoard(board);
+      displayMines(id);
     } else {
-      const board = structuredClone(gameBoard);
-      board[row][col].hidden = false;
-      setGameBoard(board);
+      const row = parseInt(id[0]);
+      const col = parseInt(id[1]);
+      const clickedField = gameBoard[row][col];
+      const actualBoard = structuredClone(gameBoard);
+      actualBoard[row][col].hidden = false;
+
+      if (clickedField.isEmpty) {
+        openAroundEmptyFields(actualBoard, row, col);
+      } else {
+        if (checkWin(actualBoard)) {
+          console.log("player won!!");
+          handleWin();
+          //TODO just for now, do I need a won state?
+        }
+      }
+
+      setGameBoard(actualBoard);
     }
   }
 
@@ -93,15 +126,8 @@ function Minesweeper() {
     setFlagsPlaced(flagsPlaced + flagCount);
     setGameBoard(board);
   }
-
-  function openEmptyFields(row, col) {
-    const board = structuredClone(gameBoard);
-    board[row][col].hidden = false;
-    openAround(board, row, col);
-    return board;
-  }
-
-  function openAround(board, x, y) {
+  
+  function openAroundEmptyFields(board, x, y) {
     const boardMin = 0;
     const boardMax = board.length;
 
@@ -117,7 +143,7 @@ function Minesweeper() {
           const field = board[checkedRowCoord][checkedColCoord];
           if (field.isEmpty && field.hidden && !field.flagged) {
             field.hidden = false;
-            openAround(board, checkedRowCoord, checkedColCoord);
+            openAroundEmptyFields(board, checkedRowCoord, checkedColCoord);
           } else if (!field.isMine && field.hidden && !field.flagged) {
             field.hidden = false;
           }

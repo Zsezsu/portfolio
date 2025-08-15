2d7134957c08c623a822485210f6688d25e34e56
Zsezsu <88768540+Zsezsu@users.noreply.github.com>
2025-08-15 10:33:47 +0200

    Refactor generateBoard.js, remove unnecessary imports and functions

--- client/src/components/Minesweeper/utils/generateBoard.js
+++ client/src/components/Minesweeper/utils/generateBoard.js
@@ -1,29 +1,3 @@
-import BOMB_IMG_CLICKED from "../assets/minesweeper_bomb_clicked.png";
-import BOMB_IMG_DEFAULT from "../assets/minesweeper_bomb_default.png";
-import HIDDEN_FIELD from "../assets/minesweeper_unopened.png";
-import FLAG_FIELD from "../assets/minesweeper_flag.png";
-import EMPTY_FIELD from "../assets/minesweeper_0.png";
-import ONE_FIELD from "../assets/minesweeper_1.png";
-import TWO_FIELD from "../assets/minesweeper_2.png";
-import THREE_FIELD from "../assets/minesweeper_3.png";
-import FOUR_FIELD from "../assets/minesweeper_4.png";
-import FIVE_FIELD from "../assets/minesweeper_5.png";
-import SIX_FIELD from "../assets/minesweeper_6.png";
-import SEVEN_FIELD from "../assets/minesweeper_7.png";
-import EIGHT_FIELD from "../assets/minesweeper_8.png";
-
-const FIELD_IMAGES = [
-  EMPTY_FIELD,
-  ONE_FIELD,
-  TWO_FIELD,
-  THREE_FIELD,
-  FOUR_FIELD,
-  FIVE_FIELD,
-  SIX_FIELD,
-  SEVEN_FIELD,
-  EIGHT_FIELD,
-];
-
 function placeBombs(board, numberOfBombs) {
   const bombPositions = [];
   while (bombPositions.length < numberOfBombs) {
@@ -54,12 +28,6 @@ function countBombsAroundFields(board) {
   }
 }
 
-function assignFieldImage(field, bombCount) {
-  field.img = FIELD_IMAGES[bombCount];
-  field.isEmpty = bombCount === 0 ? true : false;
-}
-
-//TODO: assignFieldImg, and the inside if could be arguments as callbacks, in this way I could reuse this function for checking a field around
 function checkFieldAround(board, field) {
   const boardMin = 0;
   const boardMax = board.length;
@@ -75,7 +43,6 @@ function checkFieldAround(board, field) {
       }
     }
   }
-  //assignFieldImage(field, bombCount);
   field.bombCount = bombCount;
   field.isEmpty = bombCount === 0 ? true : false;
 }
